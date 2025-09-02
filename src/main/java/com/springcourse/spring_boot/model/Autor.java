f61c71be55c7b2ee885b9eec8631bb5b1dbaac43
eliasguisolfo11 <eliasguisolfo.11@gmail.com>
2025-09-01 23:27:30 -0300

    Initial commit

--- src/main/java/com/springcourse/spring_boot/model/Autor.java
+++ src/main/java/com/springcourse/spring_boot/model/Autor.java
@@ -1,13 +1,18 @@
 package com.springcourse.spring_boot.model;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Autor {
+    @Id
+
     private Long id;
     private String nombre, apellido, telefono;
 }
