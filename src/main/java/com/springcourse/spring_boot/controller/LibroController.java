eedb646c3a1e61662170c1496324c8ed5b5efdb6
eliasguisolfo11 <eliasguisolfo.11@gmail.com>
2025-09-13 10:16:01 -0300

    .

--- src/main/java/com/springcourse/spring_boot/controller/LibroController.java
+++ src/main/java/com/springcourse/spring_boot/controller/LibroController.java
@@ -3,7 +3,6 @@ package com.springcourse.spring_boot.controller;
 
 import com.springcourse.spring_boot.model.Libros;
 import com.springcourse.spring_boot.service.ILibroService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
